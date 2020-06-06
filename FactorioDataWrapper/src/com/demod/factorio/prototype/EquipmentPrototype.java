bee839fb22c914fdbacaad7cb512c0b580375003
Steven Fontaine <sir.fon1103@gmail.com>
2020-06-06 08:06:31 -0500

    restructure

--- FactorioDataWrapper/src/com/demod/factorio/prototype/EquipmentPrototype.java
+++
@@ -1,9 +0,0 @@
-package com.demod.factorio.prototype;
-
-import org.luaj.vm2.LuaTable;
-
-public class EquipmentPrototype extends DataPrototype {
-	public EquipmentPrototype(LuaTable lua, String name, String type) {
-		super(lua, name, type);
-	}
-}
--- FactorioDataWrapper/src/com/demod/factorio/prototype/ItemPrototype.java
+++
@@ -1,9 +0,0 @@
-package com.demod.factorio.prototype;
-
-import org.luaj.vm2.LuaTable;
-
-public class ItemPrototype extends DataPrototype {
-	public ItemPrototype(LuaTable lua, String name, String type) {
-		super(lua, name, type);
-	}
-}
